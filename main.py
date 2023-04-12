151589473d0186c7646a8922593a3ce9e4369a79
pokm321 <pokm90@gmail.com>
2023-04-12 18:59:19 +0900

    Add more details to the console messages

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from PIL import Image
 import shutil
 import os
 
-original_directory = "C:/original/directory/"    # Directory containing images to convert
-processed_directory = "C:/processed/directory/"  # Directory to save converted images
+base_dir = "C:/Base/Dir/"                   # Directory containing images to convert
+processed = "Processed/"                    # Directory (inside the base_dir) to save converted images
 ratio = 16 / 9                              # Aspect ratio of converted images
 skip_redundant = True                       # Whether to skip processing images that have already been converted
 
@@ -13,26 +13,42 @@ blur = 0.15                                 # Blur level of the background
 contrast = 0.6                              # Contrast level of the background
 
 
-os.makedirs(processed_directory, exist_ok=True)
+processed_dir = base_dir + processed
+temp_dir = base_dir + "temp/"
+os.makedirs(processed_dir, exist_ok=True)
+os.makedirs(temp_dir, exist_ok=True)
 
-for file in os.listdir(original_directory):
-  if os.path.isdir(original_directory + file) or file.endswith(".bat"):
+count_processed, count_skip, count_error = 0, 0, 0
+
+for file in os.listdir(base_dir):
+  if os.path.isdir(base_dir + file) or file.endswith(".bat"):
     continue
 
-  if skip_redundant and os.path.exists(processed_directory + file):
+  if skip_redundant and os.path.exists(processed_dir + file):
+    shutil.move(processed_dir + file, temp_dir + file)
+    count_skip += 1
     continue
 
   try:
-    with Image.open(original_directory + file) as input_image:
+    with Image.open(base_dir + file) as input_image:
       if file.endswith(".gif"):
-        shutil.copyfile(original_directory + file, processed_directory + file)
+        shutil.copyfile(base_dir + file, temp_dir + file)
         #output_frames, output_duration = process_gif(input_image, ratio, blur, contrast)
         #output_frames[0].save(processed_directory + file, format='GIF', save_all=True, append_images=output_frames[1:], loop=0, duration=output_duration)
       else:
         output_image = process_image(input_image, ratio, blur, contrast)
-        output_image.save(processed_directory + file)
+        output_image.save(temp_dir + file)
+      count_processed += 1
   except Exception as e:
-    print("Error processing :", original_directory + file)
+    print("Error processing :", base_dir + file)
     print(str(e))
+    count_error += 1
 
-print("Done processing.")
+count_deleted = len(os.listdir(processed_dir))
+shutil.rmtree(processed_dir)
+os.rename(temp_dir, processed_dir)
+
+count_total = count_processed + count_skip + count_error
+print(f"Done processing!\n...Processed : {count_processed} \n...Skipped   : {count_skip}\n...Errors    : {count_error} \n...Total     : {count_total}")
+if (count_deleted):
+  print(f"\n{count_deleted} files got deleted because they no longer exist in the base directory.")
